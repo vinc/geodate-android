eea1dad7709c28c309cb09f5a2bfde856ae8817a
Vincent Ollivier <v@vinc.cc>
2016-04-14 22:35:38 +0000

    Update design

--- app/src/main/java/com/vinua/detriwidget/MainActivity.java
+++ app/src/main/java/com/vinua/detriwidget/MainActivity.java
@@ -3,7 +3,10 @@ package com.vinua.detriwidget;
 import android.content.Context;
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.text.SpannableString;
+import android.text.style.ForegroundColorSpan;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,8 +18,14 @@ public class MainActivity extends AppCompatActivity {
             Context context = getApplicationContext();
             double longitude = new ClockLocation(context).getLongitude();
             ClockTime clockTime = new ClockTime(System.currentTimeMillis() / 1000, longitude, true);
-            String text = clockTime.toString();
-            clockText.setText(text);
+            SpannableString text = new SpannableString(clockTime.toString());
+            int color = ContextCompat.getColor(context, R.color.colorPrimaryText);
+            text.setSpan(new ForegroundColorSpan(color), 0, 2, 0);
+            text.setSpan(new ForegroundColorSpan(color), 3, 5, 0);
+            text.setSpan(new ForegroundColorSpan(color), 6, 8, 0);
+            text.setSpan(new ForegroundColorSpan(color), 9, 11, 0);
+            text.setSpan(new ForegroundColorSpan(color), 12, 14, 0);
+            clockText.setText(text, TextView.BufferType.SPANNABLE);
             handler.postDelayed(textRunnable, 1000);
         }
     };
@@ -26,6 +35,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         clockText = (TextView) findViewById(R.id.textView);
-        handler.postDelayed(textRunnable, 1000);
+        handler.postDelayed(textRunnable, 0);
     }
 }
